3fffad607f589fa2abed7d92295ecd5fbf808370
qqweweqewe <golubkovivan4@gmail.com>
2024-09-24 12:35:57 +0300

    fixed mesage updates

--- module/install.sh
+++ module/install.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
 
 # init
-
-# colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
 # config defaults
 echo "friend_id=" > config.conf
-read -p "enter your locket password: " password
-echo "password=$password" >> config.conf
 
+echo "enter your locket password: " 
+read -r password
+
+echo "password=$password" >> config.conf
 
 response=$(curl -s --json "{\"password\": \"$password\"}" http://cubercube.ru:8080/api/v1/user)
 echo "id=$response" >> config.conf
 
-echo -e "your id is: \033[31m$response\033[0m. now share it with friends!"
-echo -e "${RED}password${NC} and ${RED}id${NC} are saved to ${GREEN}config.conf${NC}"
+echo "your id is: $response. now share it with friends!"
+echo "password and id are saved to config.conf"
 
 #installation
 mkdir -p ~/.config/polybar/modules/locket
 cp -r ./* ~/.config/polybar/modules/locket
 rm -rf ~/.config/polybar/modules/locket/.git ~/.config/polybar/modules/locket/README.md ~/.config/polybar/modules/locket/install.sh
 
-echo -e "${GREEN}installation complete${NC}"
+echo "--installation complete--"
--- module/polybar-locket.sh
+++ module/polybar-locket.sh
@@ -5,7 +5,7 @@ source $HOME/.config/polybar/modules/locket/config.conf
 if [ "$1" = "update" ]; then
     new_message=$(zenity --entry --title="Update Locket Message" --text="Enter your new message:")
     if [ -n "$new_message" ]; then
-        curl -s --json "{\"password\": \"$password\", \"message\": \"$new_message\"}" http://cubercube.ru:8080/api/v1/user/$id/msg
+        curl -sX PUT --json "{\"password\": \"$password\", \"msg\": \"$new_message\"}" http://cubercube.ru:8080/api/v1/user/$id/msg
     fi
 else
     # This is the normal output for the polybar module
